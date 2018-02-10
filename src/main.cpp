b760861c8737a8c9d2289adb98594d2229903506
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-02-10 22:18:10 +0100

    Création d'une fenêtre

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
 #include <GL/gl.h>
-#include <glm/glm.hpp>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
+#include "viewer.hpp"
+
+namespace input{
+  void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    std::cout << key << '\n';
+    switch (key){
+      case GLFW_KEY_ESCAPE:
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
-  std::cout << "Hello World!\n";
+  
+  if (!glfwInit()){
+    std::cerr << "Could not initialize GLFW. :(\n";
+    exit(-1);
+  }
+  
+  Viewer viewer(800, 600, "PDP");
+  glfwSetKeyCallback(viewer.getGlfwWindow(), input::key_callback);
+
+  while(!viewer.shouldClose()){
+    
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
