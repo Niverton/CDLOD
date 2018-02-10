48d3c9fe4d8abbd8890a1af711e3f0bb737172f3
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-02-10 22:59:15 +0100

    Initialization de glew

---
+++ src/glUtils.cpp
@@ -0,0 +1,16 @@
+#include "glUtils.hpp"
+#include <iostream>
+
+void glUtils::initGL(){
+  GLenum status = glewInit();
+  if (status != GLEW_OK){
+    std::cerr << "Error: could not initialize GLEW: " << glewGetErrorString(status) << ". :(\n";
+    exit(-1);
+  }
+  std::cout << "Using GLEW " << glewGetString(GLEW_VERSION) << ".\n";
+
+  if (!glewIsSupported("GL_VERSION_3_3")){
+    std::cerr << "Error: OpenGL 3.3 is not suported by your system. :(\n";
+    exit(-1);
+  }
+}
