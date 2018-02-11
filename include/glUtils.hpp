621a771092153b285ef4ccf3cd5977c6bfa69235
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-02-11 21:09:55 +0100

    Gestion des erreurs opengl

--- include/glUtils.hpp
+++ include/glUtils.hpp
@@ -1,5 +1,15 @@
 #include <GL/glew.h>
 
 namespace glUtils{
+
+  void callback_error(
+    GLenum src, 
+    GLenum type, 
+    GLuint id, 
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam);
+
   void initGL();
 }
