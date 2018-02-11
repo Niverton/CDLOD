42f1aec147117b33862d24f9e481783c3d3474d3
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-02-11 21:25:02 +0100

    Generation du maillage

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -1,6 +1,29 @@
 #include "terrain.hpp"
+#include "sphere_mesh.hpp"
 
 Terrain::Terrain(){
+  /*
+  SphereMesh sphere_generator(0.4, 10);
+  
+  vertices = sphere_generator.getVertices();
+  faces = sphere_generator.getFaces();
+  */
+  
+  type::Vertex v1;
+  type::Vertex v2;
+  type::Vertex v3;
+
+  v1.position = type::Vec3(-0.5, -0.5, 0);
+  v2.position = type::Vec3(0.5, -0.5, 0);
+  v3.position = type::Vec3(0.5, 0.5, 0);
+
+  vertices.push_back(v1);
+  vertices.push_back(v2);
+  vertices.push_back(v3);
+
+  type::Face f1(0, 1, 2);
+  faces.push_back(f1);
+
   /* Create buffer for vertices */
   glGenBuffers(1, &vertex_buffer_ID);
   if (!(glGetError() == GL_NO_ERROR)){
@@ -13,6 +36,10 @@ Terrain::Terrain(){
     sizeof(type::Vertex) * vertices.size(),
     static_cast<void*>(&vertices[0].position[0]),
     GL_DYNAMIC_DRAW);
+  if (!(glGetError() == GL_NO_ERROR)){
+    throw "Error: could not send vertex data to GC.\n";
+  }
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   /* create buffer for faces */
@@ -27,6 +54,9 @@ Terrain::Terrain(){
     sizeof(type::Face) * faces.size(),
     static_cast<void*>(&faces[0]),
     GL_STATIC_DRAW);
+  if (!(glGetError() == GL_NO_ERROR)){
+    throw "Error: could not send index data to GC.\n";
+  }
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
   glGenVertexArrays(1, &vertex_array_ID);
