44fca5b22a30fbf0157cb6569373feb9561eeac5
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-02-11 21:09:55 +0100

    Gestion des erreurs opengl

--- src/glUtils.cpp
+++ src/glUtils.cpp
@@ -1,5 +1,6 @@
 #include "glUtils.hpp"
 #include <iostream>
+#include <string>
 
 void glUtils::initGL(){
   glewExperimental=true;
@@ -13,5 +14,23 @@ void glUtils::initGL(){
   if (!glewIsSupported("GL_VERSION_3_3")){
     std::cerr << "Error: OpenGL 3.3 is not suported by your system. :(\n";
     exit(-1);
+  }
+
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback( (GLDEBUGPROC) callback_error, 0);
+}
+
+
+void glUtils::callback_error(
+  GLenum src, 
+  GLenum type, 
+  GLuint id, 
+  GLenum severity,
+  GLsizei length,
+  const GLchar* message,
+  const void* userParam){
+  if (type == GL_DEBUG_TYPE_ERROR){
+    std::cerr << "GL ERROR -" << " id: " << id << 
+    " severity: " << severity << '\t' << "msg: " << (void*) message << '\n';
   }
 }
