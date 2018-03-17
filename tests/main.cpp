cdbf8e4c6bbde6e797241b832312a1937bdd2440
Rémy Maugey <rmaugey@etu.u-bordeaux.fr>
2018-03-17 16:19:24 +0100

    Added test facilities to build system.
    Tests are built if the BUILD_TESTS cmake variable is enabled
    (default=YES). To run the tests, run 'make test'

---
+++ tests/main.cpp
@@ -0,0 +1,15 @@
+/*! \file main.cpp
+ *  \brief Entry point for all test files.
+ *  Runs test() and returns the value.
+ *  How to use:
+ *      - Write a unit test file defining the `int test()` function
+ *      - Link against this file
+ *      - ???
+ *      - Profit
+ */
+
+int test();
+
+int main(void) {
+  return test();
+}
