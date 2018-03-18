83cba2ae73942419eeeff7e12d6f5e3f5b02aab8
Rémy Maugey <rmaugey@etu.u-bordeaux.fr>
2018-03-18 19:10:01 +0100

    Test skeletton for Triangulator::GenerateGeometry

---
+++ tests/Triangulator_generate_geometry.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "Triangulator.h"
+#include "Triangulator_generate_geometry.h"
+
+bool test() {
+  //TODO
+  return true;
+}
---
+++ tests/Triangulator_generate_geometry.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "disable_gl.h"
+
