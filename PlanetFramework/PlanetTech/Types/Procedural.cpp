bfe9fc03c969a2d6293a818f5f0c0e43149b7732
Jérémi Bernard <jeremi.bernard95@gmail.com>
2018-03-19 17:41:12 +0100

    Add simplex generation

--- PlanetFramework/PlanetTech/Types/Procedural.cpp
+++ PlanetFramework/PlanetTech/Types/Procedural.cpp
@@ -1,24 +1,31 @@
+#include <array>
 #include "Procedural.h"
 #include "stdafx.h"
 #include <IL/il.h>
 #include <IL/ilu.h>
 #include "../../Texture.h"
+#include "SimplexNoise.hpp"
 
 ProceduralPlanet::ProceduralPlanet() : Planet() {
   //m_pDiffuse = new Texture("./Textures/moon8k.jpg");
   //m_pHeight = new Texture("./Textures/MoonHeight.jpg");
 
   //Compute texture here
-  
-  ILuint img;
-  ilGenImages(1, &img);
-  ilBindImage(img);
+  const int h = 2048;    //same as moonHeight
+  const int w = 4096;
 
-
-  ILuint *data = nullptr;
+  auto data = std::array<float, h*w>
+  for (int i=0; i<h; i++){
+    for (int j=0; j<w; j++){
+      data[i+j] = SimplexNoise2d(i, j);
+    }
+  }
 
   m_Radius = 1737.1f;
   m_MaxHeight = 10.7f;
+
+  m_pHeight = new Texture(data.data());
+  m_pDiffuse = &m_pHeight;
 }
 
 ProceduralPlanet::~ProceduralPlanet() {
