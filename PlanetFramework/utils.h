c589bb5bbd76a8fed65da8401b50676470cf6c3f
Rémy Maugey <rmaugey@etu.u-bordeaux.fr>
2018-03-19 15:01:59 +0100

    Added utils header file for global macros and SafeDelete

---
+++ PlanetFramework/utils.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef GLM_LEFT_HANDED
+#define GLM_LEFT_HANDED
+#endif
+
+#define SETTINGS Settings::GetInstance()
+#define WINDOW Settings::GetInstance()->Window
+#define CONTEXT Context::GetInstance()
+#define TIME Context::GetInstance()->pTime
+#define SCENE Context::GetInstance()->pScene
+#define CAMERA Context::GetInstance()->pCamera
+#define INPUT InputManager::GetInstance()
+
+template <class T>
+inline void SafeDelete(T &pObjectToDelete) {
+  if (pObjectToDelete != 0) {
+    delete (pObjectToDelete);
+    pObjectToDelete = 0;
+  }
+}
+
+// TODO move this to a custom file with a solid ruleset
+#define UINT32 unsigned int
+#define UINT unsigned int
+#define BYTE char
+#define USHORT unsigned short
+#define UCHAR unsigned char
