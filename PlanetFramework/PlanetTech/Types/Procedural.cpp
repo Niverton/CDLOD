806c88fcf373caec61850130a1db055b4405d0ce
Rémy Maugey <rmaugey@etu.u-bordeaux.fr>
2018-03-30 14:05:08 +0200

    Removed detail heightmap generation in Procedural

--- PlanetFramework/PlanetTech/Types/Procedural.cpp
+++ PlanetFramework/PlanetTech/Types/Procedural.cpp
@@ -8,7 +8,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/noise.hpp>
 
-ProceduralPlanet::ProceduralPlanet(Noise n) : Planet() {
+ProceduralPlanet::ProceduralPlanet(Noise n) {
   m_pDiffuse = new Texture("./Textures/moon8k.jpg");
   // m_pHeight = new Texture("./Textures/MoonHeight.jpg");
   // Compute texture here
@@ -28,44 +28,7 @@ ProceduralPlanet::ProceduralPlanet(Noise n) : Planet() {
     }
   }
 
-  ///*
-  int wd1 = 800;
-  int hd1 = 800;
-
-  m_dataDetail1 = new float*[hd1];
-  for (int i=0; i<hd1; i++){
-    m_dataDetail1[i] = new float[wd1];
-  }
-
-  for (int i=0; i<hd1; i++){
-    for (int j=0; j<wd1; j++){
-      glm::vec2 pos = glm::vec2(i, j);
-      float noise = glm::simplex(pos);
-      m_dataDetail1[i][j] = noise;
-    }
-  }
-
-  int wd2 = 1024;
-  int hd2 = 1024;
-
-  m_dataDetail2 = new float*[hd2];
-  for (int i=0; i<h; i++){
-    m_dataDetail2[i] = new float[wd2];
-  }
-
-  for (int i=0; i<hd2; i++){
-    for (int j=0; j<wd2; j++){
-      glm::vec2 pos = glm::vec2(i, j);
-      float noise = glm::simplex(pos);
-      m_dataDetail2[i][j] = noise;
-    }
-  }
-
-  m_pDetail1 = new Texture(m_dataDetail1, wd1, hd1);
-  m_pDetail2 = new Texture(m_dataDetail2, wd2, hd2);
-  m_pHeightDetail = new Texture(m_dataDetail1, wd1, hd1);
-  //*/
-
+  // Taken from Moon
   m_Radius = 1737.1f;
   m_MaxHeight = 10.7f;
 
