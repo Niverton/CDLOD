8b5e9168a7517e7f86c03553a65cde74e1b96358
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-03-30 19:59:42 +0200

    no morph

--- PlanetFramework/Shaders/patch.glsl
+++ PlanetFramework/Shaders/patch.glsl
@@ -48,7 +48,7 @@
 		float dist = length(TriPos-camPos);
 		float mPerc = morphFac(dist, level);
 		//morph
-		TriPos += mPerc*(r*morph.x + s*morph.y);
+		//TriPos += mPerc*(r*morph.x + s*morph.y);
 		//add height
 		Tex3 = normalize(TriPos);
 		TriPos = Tex3 * (radius + height(Tex3));
