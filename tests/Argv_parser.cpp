3c10d94a70faf4a42de69c40b03bc9b920a1797d
Niverton <niverton@live.fr>
2018-04-12 20:08:24 +0200

    Silenced compiler warning const qualifier discard
    Explicit cast from const char** to char**

--- tests/Argv_parser.cpp
+++ tests/Argv_parser.cpp
@@ -1,29 +1,29 @@
 #include "ArgvParser.h"
+#include <array>
+#include <sstream>
 
 #define ARGC 5
 
-bool test(){
-  char* argv[ARGC] =
-  { "./Argv_parser",
-    "SIMPLEX",
-    "--width=400",
-    "--max-height=0.5",
-    "--help"
-  };
+bool test() {
+  std::array<const char *, ARGC> argv = {
+      "./Argv_parser", "SIMPLEX", "--width=400", "--max-height=0.5", "--help"};
 
-  ArgvParser parser { ARGC, argv };
+  ArgvParser parser{ARGC, const_cast<char **>(argv.data())};
 
   unsigned int width = 0;
   parser.GetCmdInt<unsigned int>("--width", &width);
-  if (width != 400)    return false;
+  if (width != 400)
+    return false;
 
-  float max_height=0.0;
+  float max_height = 0.0;
   parser.GetCmdFloat("--max-height", &max_height);
-  if (max_height != 0.5)    return false;
+  if (max_height != 0.5)
+    return false;
 
   bool help = false;
   parser.GetCmdBool("--help", &help);
-  if (help != true)   return false;
+  if (help != true)
+    return false;
 
   return true;
 }
