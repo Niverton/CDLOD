5eb35f9a9893cd6b660b7f17ee55c4922aff8a2c
Niverton <niverton@live.fr>
2018-04-12 18:22:52 +0200

    Cleaned ArgvParser

--- PlanetFramework/ArgvParser.cpp
+++ PlanetFramework/ArgvParser.cpp
@@ -2,33 +2,24 @@
 #include <iostream>
 #include <sstream>
 
-ArgvParser::ArgvParser(int argc, char** argv){
-  m_Mode = std::string (argv[1]);
-  for (int i=2; i<argc; i++){
+ArgvParser::ArgvParser(int argc, char **argv) : m_Mode{argv[1]}{
+  for (int i = 2; i < argc; i++) {
     m_Tokens.emplace_back(Split(argv[i], '='));
-  }
-
-  for (auto& t: m_Tokens){
-    std::string e1, e2;
-    std::tie(e1, e2) = t;
   }
 }
 
-ArgvParser::Argv_t ArgvParser::Split(char* cmd, char sep){
+ArgvParser::Argv_t ArgvParser::Split(char *cmd, char sep) {
   std::istringstream input;
   input.str(std::string(cmd));
 
   std::vector<std::string> param;
-  for (std::string line; std::getline(input, line, sep); ){
+  for (std::string line; std::getline(input, line, sep);) {
     param.emplace_back(line);
   }
-  if (param.size() == 1){
-    //true because cmd is set even if value is nil
+  if (param.size() == 1) {
+    // true because cmd is set even if value is nil
     return std::make_tuple(param[0], "true");
   }
 
   return std::make_tuple(param[0], param[1]);
 }
-
-
-
--- PlanetFramework/ArgvParser.h
+++ PlanetFramework/ArgvParser.h
@@ -1,26 +1,27 @@
 #pragma once
 
-#include <vector>
+#include <iostream>
 #include <string>
 #include <tuple>
-#include <iostream>
+#include <vector>
 
 class ArgvParser {
   using Argv_t = std::tuple<std::string, std::string>;
+
 public:
-  ArgvParser(int argc, char** argv);
-  ~ArgvParser() {}
-  
+  ArgvParser(int argc, char **argv);
+  ~ArgvParser() = default;
+
   template <typename T>
-  void GetCmdInt(const std::string& cmd, T* ret){
-    for (auto& t: m_Tokens){
+  void GetCmdInt(const std::string &cmd, T *ret) {
+    for (auto &t : m_Tokens) {
       std::string token, value;
       std::tie(token, value) = t;
-      if (token == cmd){
+      if (token == cmd) {
         try {
           *ret = std::stoi(value);
-        }catch(...){
-          std::cerr << "Error: " << value << " is an invalid value for " 
+        } catch (...) {
+          std::cerr << "Error: " << value << " is an invalid value for "
                     << token << " parameter\n";
           std::exit(EXIT_FAILURE);
         }
@@ -30,15 +31,15 @@ public:
     *ret = 0;
   }
 
-  void GetCmdFloat(const std::string& cmd, float* ret){
-    for (auto& t: m_Tokens){
+  void GetCmdFloat(const std::string &cmd, float *ret) {
+    for (auto &t : m_Tokens) {
       std::string token, value;
       std::tie(token, value) = t;
-      if (token == cmd){
+      if (token == cmd) {
         try {
           *ret = std::stof(value);
-        }catch (...) {
-          std::cerr << "Error: " << value << " is an invalid value for " 
+        } catch (...) {
+          std::cerr << "Error: " << value << " is an invalid value for "
                     << token << " parameter\n";
           std::exit(EXIT_FAILURE);
         }
@@ -48,26 +49,27 @@ public:
     *ret = 0.0;
   }
 
-  void GetCmdBool(const std::string& cmd, bool* ret){
-    for (auto& t: m_Tokens){
+  void GetCmdBool(const std::string &cmd, bool *ret) {
+    for (auto &t : m_Tokens) {
       std::string token, value;
       std::tie(token, value) = t;
-      if (token == cmd){
-        if (value == "true"){
+      if (token == cmd) {
+        if (value == "true") {
           *ret = true;
         }
         return;
       }
     }
     *ret = false;
-
   }
 
-  std::string GetMode() { return m_Mode; }
+  std::string GetMode() {
+    return m_Mode;
+  }
+
 private:
   std::string m_Mode;
   std::vector<Argv_t> m_Tokens;
 
-
-  Argv_t Split(char* cmd, char sep);
+  Argv_t Split(char *cmd, char sep);
 };
